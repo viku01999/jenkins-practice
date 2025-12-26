2a94346792ab453acbbda35d79c0cc7979cac363
vikas <vikas.kumar@suhora.com>
2025-12-26 16:42:44 +0530

    upadted

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const greeting: string = "Hello, TypeScript Jenkins!";
-console.log(greeting);
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/api/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running successfully",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
