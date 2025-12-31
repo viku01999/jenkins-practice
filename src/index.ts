903bf48bc9f7cd6cfd8a8dd1e4e4be089823c353
vikas <vikas.kumar@suhora.com>
2025-12-31 17:26:40 +0530

    added blue green deployment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
